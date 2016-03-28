4c8b41d8d45cc4e237e25ca9af419826cf5420cb
Andy Phillipson <andy@bitpay.com>
2016-03-28 13:04:33 -0400

    Add wallet listeners and update wallet applets on changes.

--- src/js/controllers/sidebarRight.js
+++ src/js/controllers/sidebarRight.js
@@ -5,14 +5,23 @@ angular.module('copayApp.controllers').controller('sidebarRightController', func
   var self = this;
   this.applets = [];
 
+  // Applets change when the theme is changed.
   $rootScope.$on('Local/ThemeUpdated', function(event) {
     self.applets = appletService.getApplets();
   });
-/*
+
+  // Listen for changes to wallet skins and update wallet applets.
+  // TODO: manage only the change rather than refreshing the whole collection.
+  $rootScope.$on('Local/SkinUpdated', function(event) {
+    self.applets = appletService.getApplets();
+  });
+
+  // Listen for new or deleted wallets.
+  // TODO: manage only the change rather than refreshing the whole collection.
   $rootScope.$on('Local/NewFocusedWallet', function(fc) {
     self.applets = appletService.getApplets();
   });
-*/
+
   $rootScope.$on('applet-bag.drop-model', function (event, element, target, source) {
     $log.debug('drop model event: applets rearranged');
     // self.applets ordering has changed and is in sync with user arrangement.
