562cdf75d75cdf147f928ff9fce276ce00781b35
Andy Phillipson <andy@bitpay.com>
2017-02-03 02:47:56 -0500

    Introduce groups and views to data structure. Refine techan directive to accept options.

--- src/js/controllers/cards/bitcoinData.js
+++ src/js/controllers/cards/bitcoinData.js
@@ -59,6 +59,11 @@ angular.module('copayApp.controllers').controller('bitcoinDataController', funct
 
   $scope.toggleCollapse = function() {
     $scope.collapsed = !$scope.collapsed;
+
+    if (!$scope.collapsed) {
+      $rootScope.$emit('ngTechan/Render');
+    }
+
     $timeout(function() {
       $ionicScrollDelegate.resize();
       $scope.$apply();
